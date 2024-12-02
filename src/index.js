e0052e8ae6aad29b7d55c677f936cde1e87cbf89
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-12-02 15:57:28 +0530

    domain name changed

--- src/index.js
+++ src/index.js
@@ -21,11 +21,12 @@ app.use(cookieParser());
 const options = {
   origin: [
     "http://localhost:5173", // Local development URL
-    "https://quill-frontend-nine.vercel.app" // Vercel frontend URL
+    // "https://quill-frontend-nine.vercel.app", // Vercel frontend URL
+    "https://main.d3gpv0fur9hbnz.amplifyapp.com/login",
   ],
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-  preflightContinue: false,  // This ensures the CORS middleware handles the preflight request
-  optionsSuccessStatus: 204 // HTTP 204 success status for preflight
+  preflightContinue: false, // This ensures the CORS middleware handles the preflight request
+  optionsSuccessStatus: 204, // HTTP 204 success status for preflight
 };
 app.use(cors(options));
 
